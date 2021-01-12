f769bbb1b06918a1f028263ba61c663a4f7098b9
Sven Lilge <slilge@cs.toronto.edu>
2021-01-12 10:13:45 -0500

    Added general information at the beginning of each C++ source code file

--- c++/include/piecewiseconstantcurvaturemodel.h
+++ c++/include/piecewiseconstantcurvaturemodel.h
@@ -1,3 +1,16 @@
+/*
+This code implements different approaches to model the kinematics/statics
+of a two segment tendon driven continuum robot and is part of the following
+publication:
+
+How to model tendon-driven continuum robots and benchmark modelling performance
+Priyanka Rao, Quentin Peyron, Sven Lilge, Jessica Burgner-Kahrs
+frontiers in Robotics and AI 2021
+DOI: 10.3389/frobt.2020.630245
+
+Copyright (C) 2021 Continuum Robotics Laboratory, University of Toronto Mississauga
+*/
+
 #ifndef PIECEWISECONSTANTCURVATUREMODEL_H
 #define PIECEWISECONSTANTCURVATUREMODEL_H
 
